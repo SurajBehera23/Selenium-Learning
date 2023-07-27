0070c62e460ebf91d9dd05d61469e7fba9930a7c
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-27 21:23:45 +0530

    added assert

--- automateRahulShetty.py
+++ automateRahulShetty.py
@@ -17,7 +17,8 @@ driver.find_element(By.NAME, "email").send_keys("suraj.b@naapbooks.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("ProEx")
 driver.find_element(By.CSS_SELECTOR, "#exampleCheck1").click()
 driver.find_element(By.XPATH,"//input[@value='Submit']").click()
-print(driver.find_element(By.CLASS_NAME,"alert-success").text)
+msg=driver.find_element(By.CLASS_NAME,"alert-success").text
+assert "Success" in msg
 
 time.sleep(2)
 driver.minimize_window()
