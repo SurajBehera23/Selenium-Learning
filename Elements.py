0fbbac14d550b3591fcf888dc931995495ad6d13
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-27 14:38:50 +0530

    edited code again

--- Elements.py
+++ Elements.py
@@ -1,28 +1,20 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
-# Set the path to your ChromeDriver executable
-chrome_driver_path = 'path/to/chromedriver.exe'
-
-# Configure Chrome options
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-
-# Create a ChromeDriver instance
-driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+service_obj = Service("C:\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj, options=chrome_options)
 
 driver.maximize_window()
 driver.get("https://saakh.naapbooks.com/login")
 print(driver.current_url)
 print(driver.title)
-inputNumber=driver.find_element(By.XPATH,"//input[@class='ant-input ant-input-lg css-yp8pcc']")
-inputNumber.send_keys("8093479088")
+driver.find_element(By.XPATH,"//input[@name='mobileNO']").send_keys("8093479088")
 time.sleep(1)
 driver.find_element(By.XPATH,"//button[@class='sc-ewnqHT lkZuBY']").click()
 time.sleep(3)
--- automateRahulShetty.py
+++ automateRahulShetty.py
@@ -1,21 +1,11 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-
-# Set the path to your ChromeDriver executable
-chrome_driver_path = 'path/to/chromedriver.exe'
-
-# Configure Chrome options
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-
-# Create a ChromeDriver instance
-driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+service_obj = Service("C:\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj, options=chrome_options)
 
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.maximize_window()
