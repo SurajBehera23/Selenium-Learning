c629c24d52c398ee922173644fde4df918c599bc
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-08-01 17:11:53 +0530

    checkbox and radio

---
+++ Checkbox and radio.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+service_obj = Service("C:\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+getvalues = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+print(len(getvalues))
+for singlevalue in getvalues:
+    if singlevalue.get_attribute("value") == "option2":
+        singlevalue.click()
+        assert singlevalue.is_selected()
+        break
+
+getradio=driver.find_elements(By.XPATH,"//input[@type='radio']")
+
+print(len(getradio))
+for allradio in getradio:
+    if allradio.get_attribute("value")=="radio3":
+        allradio.click()
+        break
