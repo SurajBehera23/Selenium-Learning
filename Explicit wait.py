5b65f1694868a1d87634074ebaf2576dbaaae1d3
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-08-07 19:11:25 +0530

    wait practice

--- Explicit wait.py
+++ Explicit wait.py
@@ -13,26 +13,26 @@ chrome_options.add_experimental_option("detach", True)
 service_obj = Service("C:\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj, options=chrome_options)
 
+productList=["Onion - 1 Kg","Musk Melon - 1 Kg","Water Melon - 1 Kg","Almonds - 1/4 Kg"]
+printlist=[]
+
 driver.maximize_window()
 driver.implicitly_wait(10)
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.XPATH, "//input[@placeholder='Search for Vegetables and Fruits']").send_keys("on")
 time.sleep(2)
 
-#Verify added item list text
-# gettext=driver.find_elements(By.XPATH, "//div[@class='products']/div")
-# time.sleep(3)
-# for mytextlist in gettext:
-#     savetext=mytextlist.find_elements(By.XPATH,"h4").text
-#     print(savetext)
-
 
 add_cart = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 print(len(add_cart))
 for clickadd in add_cart:
-    webelement= clickadd.find_element(By.XPATH, "h4").text
+    printlist.append(clickadd.find_element(By.XPATH, "h4").text)
     clickadd.find_element(By.XPATH, "div/button").click()
-print(webelement)
+
+#comparedata
+assert printlist==productList
+
+
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 
